6d4e8466fe3bb5c2802f0bb6f3be7fbfc3e5591b
nbank <bank@neoskop.de>
2022-11-06 09:41:45 +0100

    fix stuff

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/global.scss";
+import "@fontsource/roboto";
 
 ReactDOM.render(
   <React.StrictMode>
